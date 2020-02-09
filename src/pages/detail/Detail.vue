5f580fbb29caa145e35a897cf1a974e94d90ce65
qk6665 <478649098@qq.com>
2020-02-09 19:36:39 +0800

    update detail header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './component/Banner'
+import DetailHeader from './component/Header'
 export default {
   name: 'detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 </style>
